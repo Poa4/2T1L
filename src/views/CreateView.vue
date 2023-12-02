811cd5b33bc0f8152a8e88e963fba9f6a1394ecd
MarpeTheKing <Marcus.Arpe.6259@student.uu.se>
2023-12-02 16:18:56 +0100

    Createview delen, stylen

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,4 +1,73 @@
 <template>
+      <main class="background">
+          <section class="wrapper"> 
+            <div id="first">
+                <p>
+                  Game ID: {{game_id}}
+                  <br/>
+                    Join at: <a v-bind:href="website">{{ website }} </a>
+                </p>
+            </div>
+            <div id="second">
+              <div>
+                <qrcode-vue :value="QRvalue" :size="size" level="H" /> <!--kan sätta in nya variabler på value och size för att ändra -->
+              </div>
+                <p>
+                  Join through your phone
+                </p>
+            </div>
+          </section>
+
+
+          <section class="wrapper2">
+            <div class="doubleAlign">
+              <button id="roundsButton">
+              X rounds
+            </button>
+            <button id="timeButton">
+              Y time
+            </button>
+            <button id="teamsButton">
+              Z Teams
+            </button>
+            </div>
+
+          </section>
+
+          <section class="wrapper3">
+            <div class="participentArea">
+                <p v-for="participent in participents">
+                  {{participent}}
+                </p>
+            </div>
+
+          </section>
+
+          <section>
+            <button id="startgame">
+              Start game
+            </button>
+          </section>
+
+        
+
+      </main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- <p><textarea name="textarea" id="t1" cols="30" rows="10" maxlength="20"></textarea></p> text area
   <div>
     Poll link: 
     <input type="text" v-model="pollId">
@@ -27,12 +96,13 @@
     </button>
     {{data}}
     <router-link v-bind:to="'/result/'+pollId">Check result</router-link>
-  </div>
+  </div>-->
 </template>
 
 <script>
 import io from 'socket.io-client';
 const socket = io("localhost:3000");
+import QrcodeVue from "qrcode.vue";
 
 export default {
   name: 'CreateView',
@@ -44,7 +114,13 @@ export default {
       answers: ["", ""],
       questionNumber: 0,
       data: {},
-      uiLabels: {}
+      uiLabels: {},
+      game_id: "",
+      website: "http://google.se",
+      QRvalue: "http://google.se",
+      participents: ["Participent 1","Participent 2"]
+
+      
     }
   },
   created: function () {
@@ -58,6 +134,9 @@ export default {
     )
     socket.on("pollCreated", (data) =>
       this.data = data)
+  },
+  components: {
+      QrcodeVue
   },
   methods: {
     createPoll: function () {
@@ -75,3 +154,93 @@ export default {
   }
 }
 </script>
+
+
+<style>
+.background{
+  background-color: rgb(39, 3, 63);
+}
+p{
+  font-size: 1.2em;
+  color: white;
+}
+
+.wrapper{
+  display: flex;
+  height: 8em;
+  margin-bottom: 0.7em;
+}
+.wrapper2{
+  display: flex;
+  height: 5em;
+  margin-bottom: 0.5em;
+}
+.doubleAlign{
+  width: 60%;
+  display: flex;
+  margin: auto;
+}
+.wrapper3{
+  display: flex;
+  height: 17em;
+  margin-bottom: 0.5em;
+}
+button{
+  background-color: rgb(237, 40, 135);
+  height: 4em;
+  width: 10em;
+  margin: auto;
+}
+
+#first{
+  width: 50%;
+  text-align: left;
+  padding-inline-start: 6em;
+}
+br{
+  display: block;
+  content: "";
+  margin-top: 1em;
+  text-align: top;
+}
+#second{
+  width: 50%;
+  display: flex;
+  padding-inline-end: 6em;
+}
+#second div{
+  background-color: white;
+  height: 100px;
+  width: 100px;
+  margin: auto;
+}
+#second p{
+  margin: auto;
+  margin-right: 30%;
+  margin-top: 1.5em;
+}
+a {
+  font-size: 1em;
+  color: white;
+}
+.participentArea{
+  height: 99%;
+  width: 60%;
+  background-color: rgb(197, 196, 196);
+  margin: auto;
+}
+.participentArea p{
+  font-size: 1em;
+  color: rgb(0, 0, 0);
+  text-align: left;
+  margin-left: 0.5em;
+}
+#startgame{
+  margin-bottom: 1em;
+}
+
+
+
+
+
+</style>
