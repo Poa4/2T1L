ede5e80b6d61b6d78cc6a68653cbf3a3cb0150b3
tildeadler <tilde.adler@outlook.com>
2023-12-26 14:27:02 +0100

    CSS Startsida

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -113,7 +113,7 @@ export default {
     },
     chooseAvatar: function(avatar) {
         this.chosenAvatar = avatar;
-        console.log(this.chosenAvatar);
+        
     },
     generateRandomAvatar: function() {
       fetch("https://emoji-api.com/emojis?access_key=bd609ab5841ff29f856c7ce1ce62a1492bb00858\n")
